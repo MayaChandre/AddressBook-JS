44e5115113a97047702ea836c1065398aebe851b
mayavathi <mayachandre27@gmail.com>
2021-08-07 02:51:04 -0400

    Added:Ability to find existing contact

--- addressbook.js
+++ addressbook.js
@@ -1,49 +1,120 @@
 
 
-//Validate contacts in address book
-let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
-let addressRegex = RegExp("^[A-Za-z]{4,}$");
-let cityStateRegex = RegExp("^[A-Za-z]{4,}$");
-let zipRegex = RegExp("^[1-9]{1}[0-9]{2}[ ]?[0-9]{3}$");
-let phoneNumberRegex = RegExp("^[1-9]{1}[0-9]{9}$");
-let emailRegex = RegExp("^([a-z]+)([0-9])*([_+-.]{1}[a-z0-9]+)*(@)([a-z0-9]+)[.]([a-z]{2,})([.][a-z]{2}){0,1}$");
+
 class AddressBook {
-    constructor(...params) {
-        if (nameRegex.test(params[0]))
-            this.firstName = params[0];
-        else throw "Invalid First Name!!";
-        if (nameRegex.test(params[1]))
-            this.lastName = params[1];
-        else throw "Invalid Last Name!!";
-        if (addressRegex.test(params[2]))
-            this.address = params[2];
-        else throw "Invalid Address!!";
-        if (cityStateRegex.test(params[3]))
-            this.city = params[3];
-        else throw "Invalid City!!";
-        if (cityStateRegex.test(params[4]))
-            this.state = params[4];
-        else throw "Invalid State!!";
-        if (zipRegex.test(params[5]))
-            this.zip = params[5];
-        else throw "Invalid Zip!!";
-        if (phoneNumberRegex.test(params[6]))
-            this.phoneNumber = params[6];
-        else throw "Invalid Phone Number!!";
-        if (emailRegex.test(params[7]))
-            this.email = params[7];
-        else throw "Invalid Email!!";
+    firstname;
+    lastname;
+    address;
+    city;
+    state;
+    zipcode;
+    phoneNumber;
+    emailId;
+
+    constructor(...parameters) {
+        this.firstname = parameters[0];
+        this.lastname = parameters[1];
+        this.address = parameters[2];
+        this.city = parameters[3];
+        this.state = parameters[4];
+        this.zipcode = parameters[5];
+        this.phoneNumber = parameters[6];
+        this.emailId = parameters[7];
     }
 
-    toString()
-     {
-        return "First Name : " + this.firstName + ", Last Name : " + this.lastName + ", Address : " + this.address + ", City : " + this.city + ", State : " + this.state + ", Zip : " + this.zip + ", Phone Number : " + this.phoneNumber + ", Email : " + this.email;
+    get firstname() { return this._firstname; }
+    set firstname(firstname) {
+        let pattern1 = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (pattern1.test(firstname))
+            this._firstname = firstname;
+        else console.log('FirstName is Incorrect!');
+
+    }
+
+    get lastname() { return this._lastname; }
+    set lastname(lastname) {
+        let pattern2 = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if (pattern2.test(lastname))
+            this._lastname = lastname;
+        else console.log('LastName is Incorrect!');
+    }
+
+    get address() { return this._address; }
+    set address(address) {
+        let pattern3 = RegExp('^[A-Z]{1}[a-z]{4,}$');
+        if (pattern3.test(address))
+            this._address = address;
+        else console.log('Address is Incorrect!');
+    }
+
+    get city() { return this._city; }
+    set city(city) {
+        let pattern4 = RegExp('^[A-Z]{1}[a-z]{4,}$');
+        if (pattern4.test(city))
+            this._city = city;
+        else console.log('City is Incorrect!');
+    }
+
+    get state() { return this._state; }
+    set state(state) {
+        let pattern5 = RegExp('^[A-Z]{1}[a-z]{4,}$');
+        if (pattern5.test(state))
+            this._state = state;
+        else console.log('State is Incorrect!');
+    }
+
+    get zipcode() { return this._zipcode; }
+    set zipcode(zipcode) {
+        let pattern6 = RegExp('^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$');
+        if (pattern6.test(zipcode))
+            this._zipcode = zipcode;
+        else console.log('Zipcode is Incorrect!');
+    }
+
+    get phoneNumber() { return this._phoneNumber; }
+    set phoneNumber(phoneNumber) {
+        let pattern7 = RegExp('^[+][0-9]{1,}\\s?[1-9]{1}[0-9]{9}$');
+        if (pattern7.test(phoneNumber))
+            this._phoneNumber = phoneNumber;
+        else console.log('PhoneNumber is Incorrect!');
+    }
+
+    get emailId() { return this._emailId; }
+    set emailId(emailId) {
+        let pattern8 = RegExp('^[A-Z a-z 0-9]+([._+-][0-9 a-z A-Z]+)*@[0-9 a-z A-Z]+.[a-z A-Z]{2,3}([.][a-z A-Z]{2})*$');
+        if (pattern8.test(emailId))
+            this._emailId = emailId;
+        else console.log('EmailId is Incorrect!');
+    }
+
+    toString() {
+        return "firstname= " + this.firstname + ", lastname = " + this.lastname + ", address = " + this.address +
+            ", city = " + this.city + ", state = " + this.state + ", zipcode = " + this.zipcode +
+            ", phoneNumber = " + this.phoneNumber + ", emailId = " + this.emailId;
     }
 }
 
-try {
-    let addressBook = new AddressBook("Maya", "Chandre", "Thane", "Mumbai", "Maharashtra", "411057",  "9785461230", "mayachandre@gmail.com");
-    console.log(addressBook.toString());
-} catch(error) {
-    console.error(error);
+let contactsArr = new Array();
+const addressbook = new AddressBook("Mark", "Wood", "Saintzeus", "Kansas", "Missouri", "100245", "+01 5690125689", "mark.wood@orkut.com");
+contactsArr.push(addressbook);
+
+const addressbook2 = new AddressBook("Hope", "Launther", "Victorhall", "Frankfurt", "Kentucky", "100461", "+01 5213409871", "taylor.l@google.com");
+contactsArr.push(addressbook2);
+
+const addressbook3 = new AddressBook("Hope", "Tyson", "Crossmall", "Blueminton", "Indiana", "430016", "+81 7801456780", "hope.ty@gmail.com");
+contactsArr.push(addressbook3);
+
+console.log(contactsArr.length);
+for (var i = 0; i < contactsArr.length; i++) {
+    console.log(contactsArr[i]);
 }
+
+//To update the Entries in the array
+let result = contactsArr.filter((e) => e.firstname == 'Hope');
+console.log(result);
+
+if (result && result.length > 0) {
+    result[0].firstname = 'Taylor';
+    result[0].address = 'Newvilla';
+}
+console.log("New Array after Updation: " + contactsArr);
